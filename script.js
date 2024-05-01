0f46761d781551e48890285a58ce2393679a2fa4
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-05-01 23:23:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+gsap.to("#box1", {
+    x: 700,
+    duration: 2,
+    delay: 1,
+    rotate: 360, 
+    backgroundColor: "blue",
+    borderRadius:"50%"
+})
+
+// gsap.from("#box1", {
+//     x: 700,
+//     duration: 2,
+//     delay: 1
+// })
+
+// gsap.to("#box2", {
+//     x: 500,
+//     y: 200,
+//     duration: 2,
+//     delay: 1
+// })
