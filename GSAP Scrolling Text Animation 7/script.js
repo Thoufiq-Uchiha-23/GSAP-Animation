91c76856bf8816db63ea52eec2764e179e011864
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-05-11 19:24:06 +0530

    Made it Prettier

--- GSAP Scrolling Text Animation 7/script.js
+++ GSAP Scrolling Text Animation 7/script.js
@@ -1,30 +1,30 @@
 function marqueAnimation() {
-    window.addEventListener("wheel", function(dets) {
-        if(dets.deltaY>0) {
-            gsap.to(".marque", {
-                transform:"translateX(-200%)",
-                // -1 here means infinity
-                repeat:-1,
-                duration:4,
-                ease: "none"
-            })
-            gsap.to(".marque img", {
-                rotate: 180
-            })
-        }else {
-            gsap.to(".marque", {
-                transform:"translateX(0%)",
-                // -1 here means infinity
-                repeat:-1,
-                duration:4,
-                ease: "none"
-            })
-    
-            gsap.to(".marque img", {
-                rotate: 0
-            })
-        }
-    })
+  window.addEventListener("wheel", function (dets) {
+    if (dets.deltaY > 0) {
+      gsap.to(".marque", {
+        transform: "translateX(-200%)",
+        // -1 here means infinity
+        repeat: -1,
+        duration: 4,
+        ease: "none",
+      });
+      gsap.to(".marque img", {
+        rotate: 180,
+      });
+    } else {
+      gsap.to(".marque", {
+        transform: "translateX(0%)",
+        // -1 here means infinity
+        repeat: -1,
+        duration: 4,
+        ease: "none",
+      });
+
+      gsap.to(".marque img", {
+        rotate: 0,
+      });
+    }
+  });
 }
 
-marqueAnimation()
+marqueAnimation();
