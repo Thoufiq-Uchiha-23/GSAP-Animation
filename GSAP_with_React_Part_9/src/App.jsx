a73a49ebf18940386b6aa6ba103dbc46a906a15c
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-05-24 18:18:53 +0530

    Learnt to animate using States, and random and scope

--- GSAP_with_React_Part_9/src/App.jsx
+++ GSAP_with_React_Part_9/src/App.jsx
@@ -1,37 +1,76 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 
-const App = () => {
-  const container = useRef()
+// Used on Circle to animate based on Random values
+// const App = () => {
+//   // GSAP Utils
+//   const container = useRef()
+//   const [circle, setCircle] = useState(0)
+//   // gsap.utils.random(starting pt, ending pt, range divisible by n)
+//   const random = gsap.utils.random(-500, 500, 100)
 
-  useGSAP(() => {
-    gsap.from(".box", {
-      rotate: 360,
-      scale: 0,
-      duration: 1,
-      opacity: 0,
-      delay: 0.2
-    })
-  }, {scope: container})
+//   useGSAP(() => {
+//     gsap.to(".circle", {
+//       x:circle,
+//       duration:0.5,
+//     })
+//   }, [circle])
+
+//   // useGSAP(() => {
+//   //   gsap.from(".box", {
+//   //     rotate: 360,
+//   //     scale: 0,
+//   //     duration: 1,
+//   //     opacity: 0,
+//   //     delay: 0.2
+//   //   })
+//   // }, {scope: container})
   
-  return (
-    <main>
-      {/* // using Scope property and useRef and useGSAP hook */}
-      {/* <div ref={container} className="container">
-        <div className="circle"></div>
-        <div className="box"></div>
-      </div>
-      <div className="kuch">
-        <div className="circle"></div>
-        <div className="box"></div>
-      </div> */}
+//   return (
+//     <main>
+//       {/* // using Scope property and useRef and useGSAP hook */}
+//       {/* <div ref={container} className="container">
+//         <div className="circle"></div>
+//         <div className="box"></div>
+//       </div>
+//       <div className="kuch">
+//         <div className="circle"></div>
+//         <div className="box"></div>
+//       </div> */}
 
-      <button>
-        Animate
-      </button>
-      <div className="circle"></div>
-    </main>
+//       <button onClick={() => {
+//         setCircle(random)
+//       }}>Animate</button>
+//       <div className="circle"></div>
+//     </main>
+//   )
+// }
+
+// Used to Animate Box based on Random value
+const App = () => {
+  // const randomX = gsap.utils.random(-500, 500, 100)
+  // const rotateX = gsap.utils.random(-360, 360, 30)
+  // const [xValue, setXValue] = useState(0)
+  // const [roti, setRoti] = useState(0)
+  // const boxRef = useRef()
+
+  // useGSAP(() => {
+  //   gsap.to(boxRef.current, {
+  //     x:xValue,
+  //     duration:1,
+  //     rotate: roti,
+  //   })
+  // }, [xValue, roti] )
+
+  return (
+    // <main>
+    //   <button onClick={() => {
+    //     setXValue(randomX)
+    //     setRoti(rotateX)
+    //   }}>Animate</button>
+    //   <div className='box' ref={boxRef}></div>
+    // </main>
   )
 }
 
